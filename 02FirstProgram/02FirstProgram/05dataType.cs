73fab74c302c21cda47813375cfd1014d6417487
Yuwen Hu <yhu71@ford.com>
2021-10-29 17:23:31 +0800

    following syllabus to 07

---
+++ 02FirstProgram/02FirstProgram/05dataType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _CsharpBasicbyHEHE
+{
+    class dataType
+    {
+        static void Main(string[] args)
+        {
+            int intA = 1;
+        }
+    }
+}
